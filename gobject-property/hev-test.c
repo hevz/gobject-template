085222b67a3e25924f2a490538f476881098934f
Heiher <admin@heiher.info>
2012-04-22 17:06:33 +0800

    Update: add gobject-property

---
+++ gobject-property/hev-test.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : hev-test.c
+ Author      : Heiher <admin@heiher.info>
+ Version     : 0.0.2
+ Copyright   : Copyright (c) 2012 everyone.
+ Description : 
+ ============================================================================
+ */
+
+#include "hev-iobj.h"
+
+int main(int argc, char *argv[])
+{
+	GObject *iobj = NULL;
+	gchar *name = NULL;
+
+	g_type_init();
+
+	iobj = hev_iobj_new();
+	g_return_val_if_fail(G_IS_OBJECT(iobj), -1);
+	g_object_set(iobj, "name", "Hello", NULL);
+	g_object_get(iobj, "name", &name, NULL);
+	g_debug("-- name: %s --", name);
+	g_object_unref(iobj);
+
+	return 0;
+}
