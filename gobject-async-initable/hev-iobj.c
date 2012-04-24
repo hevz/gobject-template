a0a8856ef176292e745e2982faec9fea732cc718
Heiher <admin@heiher.info>
2012-04-24 10:40:35 +0800

    Update: gobject-async-initable

--- gobject-async-initable/hev-iobj.c
+++ gobject-async-initable/hev-iobj.c
@@ -12,11 +12,19 @@
 
 #define HEV_IOBJ_GET_PRIVATE(obj)	(G_TYPE_INSTANCE_GET_PRIVATE((obj), HEV_TYPE_IOBJ, HevIObjPrivate))
 
+enum
+{
+	STEP_ZERO,
+	STEP_FIRST,
+	STEP_SECOND,
+	N_STEPS
+};
+
 typedef struct _HevIObjPrivate HevIObjPrivate;
 
 struct _HevIObjPrivate
 {
-	gchar c;
+	gint step;
 };
 
 static void hev_iobj_async_initable_iface_init(GAsyncInitableIface *iface);
@@ -90,21 +98,41 @@ static void hev_iobj_init(HevIObj *self)
 	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE(self);
 
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+
+	priv->step = STEP_ZERO;
 }
 
 static void hev_iobj_async_initable_init_async(GAsyncInitable *initable,
 			gint io_priority, GCancellable *cancellable,
 			GAsyncReadyCallback callback, gpointer user_data)
 {
+	HevIObj *self = HEV_IOBJ(initable);
+	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE(self);
 	GSimpleAsyncResult *simple = NULL;
 
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
 	simple = g_simple_async_result_new(G_OBJECT(initable),
-				callback, user_data, NULL);
-	g_simple_async_result_set_check_cancellable(simple, cancellable);
-	g_simple_async_result_complete(simple);
-	g_object_unref(simple);
+				callback, user_data, hev_iobj_async_initable_init_async);
+
+	switch(priv->step)
+	{
+	case STEP_ZERO:
+		g_debug("%s:%d[%s]=>(%s)", __FILE__, __LINE__,
+					__FUNCTION__, "Zero");
+		priv->step = STEP_FIRST;
+	case STEP_FIRST:
+		g_debug("%s:%d[%s]=>(%s)", __FILE__, __LINE__,
+					__FUNCTION__, "Frist");
+		priv->step = STEP_SECOND;
+	case STEP_SECOND:
+		g_debug("%s:%d[%s]=>(%s)", __FILE__, __LINE__,
+					__FUNCTION__, "Second");
+		g_simple_async_result_set_check_cancellable(simple, cancellable);
+		g_simple_async_result_complete(simple);
+		g_object_unref(simple);
+		break;
+	}
 }
 
 static gboolean hev_iobj_async_initable_init_finish(GAsyncInitable *initable,
@@ -113,7 +141,8 @@ static gboolean hev_iobj_async_initable_init_finish(GAsyncInitable *initable,
 	g_debug("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
 	g_return_val_if_fail(g_simple_async_result_is_valid(result,
-					G_OBJECT(initable), NULL), FALSE);
+					G_OBJECT(initable), hev_iobj_async_initable_init_async),
+				FALSE);
 	if(g_simple_async_result_propagate_error(G_SIMPLE_ASYNC_RESULT(result),
 					error))
 	  return FALSE;
