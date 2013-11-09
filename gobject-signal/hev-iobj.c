147f482dcde80e7514d2134719774a7c6d99e4aa
Heiher <root@heiher.info>
2013-11-09 22:24:51 +0800

    gobject-signal: Update style.

--- gobject-signal/hev-iobj.c
+++ gobject-signal/hev-iobj.c
@@ -1,9 +1,8 @@
 /*
  ============================================================================
  Name        : hev-iobj.c
- Author      : Heiher <admin@heiher.info>
- Version     : 0.0.2
- Copyright   : Copyright (C) 2012 everyone.
+ Author      : Heiher <root@heiher.info>
+ Copyright   : Copyright (C) 2013 everyone.
  Description : 
  ============================================================================
  */
@@ -11,19 +10,19 @@
 #include "hev-iobj.h"
 
 #define HEV_IOBJ_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), \
-                HEV_TYPE_IOBJ, HevIObjPrivate))
+				HEV_TYPE_IOBJ, HevIObjPrivate))
 
 enum
 {
-    SIG_READY,
-    LAST_SIGNAL
+	SIG_READY,
+	LAST_SIGNAL
 };
 
 typedef struct _HevIObjPrivate HevIObjPrivate;
 
 struct _HevIObjPrivate
 {
-    gchar c;
+	gchar c;
 };
 
 static guint hev_iobj_signals[LAST_SIGNAL] = { 0 };
@@ -35,92 +34,92 @@ static void hev_iobj_real_ready (HevIObj *self);
 static void
 hev_iobj_dispose (GObject *obj)
 {
-    HevIObj *self = HEV_IOBJ (obj);
-    HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
+	HevIObj *self = HEV_IOBJ (obj);
+	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    G_OBJECT_CLASS (hev_iobj_parent_class)->dispose (obj);
+	G_OBJECT_CLASS (hev_iobj_parent_class)->dispose (obj);
 }
 
 static void
 hev_iobj_finalize (GObject *obj)
 {
-    HevIObj *self = HEV_IOBJ (obj);
-    HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
+	HevIObj *self = HEV_IOBJ (obj);
+	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    G_OBJECT_CLASS (hev_iobj_parent_class)->finalize (obj);
+	G_OBJECT_CLASS (hev_iobj_parent_class)->finalize (obj);
 }
 
 static GObject *
 hev_iobj_constructor (GType type,
-            guint n,
-            GObjectConstructParam *param)
+			guint n,
+			GObjectConstructParam *param)
 {
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    return G_OBJECT_CLASS (hev_iobj_parent_class)->
-        constructor (type, n, param);
+	return G_OBJECT_CLASS (hev_iobj_parent_class)->
+		constructor (type, n, param);
 }
 
 static void
 hev_iobj_constructed (GObject *obj)
 {
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    G_OBJECT_CLASS (hev_iobj_parent_class)->constructed (obj);
+	G_OBJECT_CLASS (hev_iobj_parent_class)->constructed (obj);
 
-    /* Emit signal */
-    g_signal_emit (obj, hev_iobj_signals[SIG_READY], 0);
+	/* Emit signal */
+	g_signal_emit (obj, hev_iobj_signals[SIG_READY], 0);
 }
 
 static void
 hev_iobj_class_init (HevIObjClass *klass)
 {
-    GObjectClass *obj_class = G_OBJECT_CLASS (klass);
+	GObjectClass *obj_class = G_OBJECT_CLASS (klass);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    obj_class->constructor = hev_iobj_constructor;
-    obj_class->constructed = hev_iobj_constructed;
-    obj_class->dispose = hev_iobj_dispose;
-    obj_class->finalize = hev_iobj_finalize;
+	obj_class->constructor = hev_iobj_constructor;
+	obj_class->constructed = hev_iobj_constructed;
+	obj_class->dispose = hev_iobj_dispose;
+	obj_class->finalize = hev_iobj_finalize;
 
-    klass->ready = hev_iobj_real_ready;
+	klass->ready = hev_iobj_real_ready;
 
-    /* Signals */
-    hev_iobj_signals[SIG_READY] = g_signal_new ("ready",
-                G_TYPE_FROM_CLASS (klass),
-                G_SIGNAL_RUN_LAST,
-                G_STRUCT_OFFSET (HevIObjClass, ready),
-                NULL, NULL,
-                g_cclosure_marshal_VOID__VOID,
-                G_TYPE_NONE, 0);
+	/* Signals */
+	hev_iobj_signals[SIG_READY] = g_signal_new ("ready",
+				G_TYPE_FROM_CLASS (klass),
+				G_SIGNAL_RUN_LAST,
+				G_STRUCT_OFFSET (HevIObjClass, ready),
+				NULL, NULL,
+				g_cclosure_marshal_VOID__VOID,
+				G_TYPE_NONE, 0);
 
-    g_type_class_add_private (klass, sizeof (HevIObjPrivate));
+	g_type_class_add_private (klass, sizeof (HevIObjPrivate));
 }
 
 static void
 hev_iobj_init (HevIObj *self)
 {
-    HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
+	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 }
 
 GObject *
 hev_iobj_new (void)
 {
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    return g_object_new (HEV_TYPE_IOBJ, NULL);
+	return g_object_new (HEV_TYPE_IOBJ, NULL);
 }
 
 static void
 hev_iobj_real_ready (HevIObj *self)
 {
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 }
 
--- gobject-signal/hev-iobj.h
+++ gobject-signal/hev-iobj.h
@@ -1,9 +1,8 @@
 /*
  ============================================================================
  Name        : hev-iobj.h
- Author      : Heiher <admin@heiher.info>
- Version     : 0.0.2
- Copyright   : Copyright (C) 2012 everyone.
+ Author      : Heiher <root@heiher.info>
+ Copyright   : Copyright (C) 2013 everyone.
  Description : 
  ============================================================================
  */
@@ -17,30 +16,30 @@ G_BEGIN_DECLS
 
 #define HEV_TYPE_IOBJ (hev_iobj_get_type ())
 #define HEV_IOBJ(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
-                HEV_TYPE_IOBJ, HevIObj))
+				HEV_TYPE_IOBJ, HevIObj))
 #define HEV_IS_IOBJ(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
-                HEV_TYPE_IOBJ))
+				HEV_TYPE_IOBJ))
 #define HEV_IOBJ_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), \
-                HEV_TYPE_IOBJ, HevIObjClass))
+				HEV_TYPE_IOBJ, HevIObjClass))
 #define HEV_IS_IOBJ_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), \
-                HEV_TYPE_IOBJ))
+				HEV_TYPE_IOBJ))
 #define HEV_IOBJ_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), \
-                HEV_TYPE_IOBJ, HevIObjClass))
+				HEV_TYPE_IOBJ, HevIObjClass))
 
 typedef struct _HevIObj HevIObj;
 typedef struct _HevIObjClass HevIObjClass;
 
 struct _HevIObj
 {
-    GObject parent_instance;
+	GObject parent_instance;
 };
 
 struct _HevIObjClass
 {
-    GObjectClass parent_class;
-    
-    /* Signal handlers */
-    void (*ready) (HevIObj *self);
+	GObjectClass parent_class;
+	
+	/* Signal handlers */
+	void (*ready) (HevIObj *self);
 };
 
 GType hev_iobj_get_type (void);
