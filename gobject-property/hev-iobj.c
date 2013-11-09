c21afbd96044bc019cc924acf586d8e30f5dae10
Heiher <root@heiher.info>
2013-11-09 22:23:13 +0800

    gobject-property: Update style.

--- gobject-property/hev-iobj.c
+++ gobject-property/hev-iobj.c
@@ -1,9 +1,8 @@
 /*
  ============================================================================
  Name        : hev-iobj.c
- Author      : Heiher <admin@heiher.info>
- Version     : 0.0.2
- Copyright   : Copyright (C) 2012 everyone.
+ Author      : Heiher <root@heiher.info>
+ Copyright   : Copyright (C) 2013 everyone.
  Description : 
  ============================================================================
  */
@@ -11,20 +10,20 @@
 #include "hev-iobj.h"
 
 #define HEV_IOBJ_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), \
-                HEV_TYPE_IOBJ, HevIObjPrivate))
+				HEV_TYPE_IOBJ, HevIObjPrivate))
 
 enum
 {
-    PROP_ZERO,
-    PROP_NAME,
-    N_PROPERTIES
+	PROP_ZERO,
+	PROP_NAME,
+	N_PROPERTIES
 };
 
 typedef struct _HevIObjPrivate HevIObjPrivate;
 
 struct _HevIObjPrivate
 {
-    gchar *name;
+	gchar *name;
 };
 
 static GParamSpec *hev_iobj_properties[N_PROPERTIES] = { NULL };
@@ -34,121 +33,121 @@ G_DEFINE_TYPE (HevIObj, hev_iobj, G_TYPE_OBJECT);
 static void
 hev_iobj_dispose (GObject *obj)
 {
-    HevIObj *self = HEV_IOBJ (obj);
-    HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
+	HevIObj *self = HEV_IOBJ (obj);
+	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    G_OBJECT_CLASS (hev_iobj_parent_class)->dispose (obj);
+	G_OBJECT_CLASS (hev_iobj_parent_class)->dispose (obj);
 }
 
 static void
 hev_iobj_finalize (GObject *obj)
 {
-    HevIObj *self = HEV_IOBJ (obj);
-    HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
+	HevIObj *self = HEV_IOBJ (obj);
+	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    G_OBJECT_CLASS (hev_iobj_parent_class)->finalize (obj);
+	G_OBJECT_CLASS (hev_iobj_parent_class)->finalize (obj);
 }
 
 static GObject *
 hev_iobj_constructor (GType type,
-            guint n,
-            GObjectConstructParam *param)
+			guint n,
+			GObjectConstructParam *param)
 {
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    return G_OBJECT_CLASS (hev_iobj_parent_class)->
-        constructor (type, n, param);
+	return G_OBJECT_CLASS (hev_iobj_parent_class)->
+		constructor (type, n, param);
 }
 
 static void
 hev_iobj_constructed (GObject *obj)
 {
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    G_OBJECT_CLASS (hev_iobj_parent_class)->constructed (obj);
+	G_OBJECT_CLASS (hev_iobj_parent_class)->constructed (obj);
 }
 
 static void
 hev_iobj_get_property (GObject *obj, guint id,
-            GValue *value, GParamSpec *pspec)
+			GValue *value, GParamSpec *pspec)
 {
-    HevIObj *self = HEV_IOBJ (obj);
-    HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
+	HevIObj *self = HEV_IOBJ (obj);
+	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    switch (id) {
-    case PROP_NAME:
-        g_value_set_string (value, priv->name);
-        break;
-    default:
-        G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, id, pspec);
-        break;
-    }
+	switch (id) {
+	case PROP_NAME:
+		g_value_set_string (value, priv->name);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, id, pspec);
+		break;
+	}
 }
 
 static void
 hev_iobj_set_property (GObject *obj, guint id,
-            const GValue *value, GParamSpec *pspec)
+			const GValue *value, GParamSpec *pspec)
 {
-    HevIObj *self = HEV_IOBJ (obj);
-    HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
+	HevIObj *self = HEV_IOBJ (obj);
+	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    switch (id) {
-    case PROP_NAME:
-        if (priv->name)
-          g_free (priv->name);
-        priv->name = g_strdup (g_value_get_string(value));
-        break;
-    default:
-        G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, id, pspec);
-        break;
-    }
+	switch (id) {
+	case PROP_NAME:
+		if (priv->name)
+		  g_free (priv->name);
+		priv->name = g_strdup (g_value_get_string(value));
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, id, pspec);
+		break;
+	}
 }
 
 static void
 hev_iobj_class_init (HevIObjClass *klass)
 {
-    GObjectClass *obj_class = G_OBJECT_CLASS (klass);
+	GObjectClass *obj_class = G_OBJECT_CLASS (klass);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    obj_class->constructor = hev_iobj_constructor;
-    obj_class->constructed = hev_iobj_constructed;
-    obj_class->dispose = hev_iobj_dispose;
-    obj_class->finalize = hev_iobj_finalize;
-    obj_class->get_property = hev_iobj_get_property;
-    obj_class->set_property = hev_iobj_set_property;
+	obj_class->constructor = hev_iobj_constructor;
+	obj_class->constructed = hev_iobj_constructed;
+	obj_class->dispose = hev_iobj_dispose;
+	obj_class->finalize = hev_iobj_finalize;
+	obj_class->get_property = hev_iobj_get_property;
+	obj_class->set_property = hev_iobj_set_property;
 
-    /* Properties */
-    hev_iobj_properties[PROP_NAME] =
-        g_param_spec_string ("name", "Name", "Name",
-                    NULL, G_PARAM_READWRITE);
-    g_object_class_install_properties (obj_class, N_PROPERTIES,
-                hev_iobj_properties);
+	/* Properties */
+	hev_iobj_properties[PROP_NAME] =
+		g_param_spec_string ("name", "Name", "Name",
+					NULL, G_PARAM_READWRITE);
+	g_object_class_install_properties (obj_class, N_PROPERTIES,
+				hev_iobj_properties);
 
-    g_type_class_add_private (klass, sizeof (HevIObjPrivate));
+	g_type_class_add_private (klass, sizeof (HevIObjPrivate));
 }
 
 static void
 hev_iobj_init (HevIObj *self)
 {
-    HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
+	HevIObjPrivate *priv = HEV_IOBJ_GET_PRIVATE (self);
 
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 }
 
 GObject *
 hev_iobj_new (void)
 {
-    g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
+	g_debug ("%s:%d[%s]", __FILE__, __LINE__, __FUNCTION__);
 
-    return g_object_new (HEV_TYPE_IOBJ, NULL);
+	return g_object_new (HEV_TYPE_IOBJ, NULL);
 }
 
--- gobject-property/hev-test.c
+++ gobject-property/hev-test.c
@@ -1,9 +1,8 @@
 /*
  ============================================================================
  Name        : hev-test.c
- Author      : Heiher <admin@heiher.info>
- Version     : 0.0.2
- Copyright   : Copyright (c) 2012 everyone.
+ Author      : Heiher <root@heiher.info>
+ Copyright   : Copyright (c) 2013 everyone.
  Description : 
  ============================================================================
  */
@@ -13,17 +12,17 @@
 int
 main (int argc, char *argv[])
 {
-    GObject *iobj = NULL;
-    gchar *name = NULL;
+	GObject *iobj = NULL;
+	gchar *name = NULL;
 
-    g_type_init ();
+	g_type_init ();
 
-    iobj = hev_iobj_new ();
-    g_return_val_if_fail (G_IS_OBJECT (iobj), -1);
-    g_object_set (iobj, "name", "Hello", NULL);
-    g_object_get (iobj, "name", &name, NULL);
-    g_debug ("-- name: %s --", name);
-    g_object_unref (iobj);
+	iobj = hev_iobj_new ();
+	g_return_val_if_fail (G_IS_OBJECT (iobj), -1);
+	g_object_set (iobj, "name", "Hello", NULL);
+	g_object_get (iobj, "name", &name, NULL);
+	g_debug ("-- name: %s --", name);
+	g_object_unref (iobj);
 
-    return 0;
+	return 0;
 }
